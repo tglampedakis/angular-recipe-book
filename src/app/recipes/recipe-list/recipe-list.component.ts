5e07054ec0ce581573d6a4ce5408438f244f0f48
Glampedakis Theodore <tglampedakis@users.noreply.github.com>
2021-02-09 17:52:12 +0200

    Initial Commit - Basic Components Setup

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test.', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F06%2F19%2FEasy-Meatloaf.jpg&q=85')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
