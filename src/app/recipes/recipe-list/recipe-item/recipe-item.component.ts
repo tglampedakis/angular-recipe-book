b7492f3ece5178ddb58f8fb0f388a14fa1eaef4a
Glampedakis Theodore <tglampedakis@users.noreply.github.com>
2021-02-12 16:25:02 +0200

    Added Routing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RecipeService } from '../../recipe.service';
 
 import { Recipe } from '../../recipes.model';
 
@@ -10,14 +9,11 @@ import { Recipe } from '../../recipes.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
